8a2eaee376fc929cb0c4534964d4a5c6d1943feb
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-11-05 19:07:06 +0000

    TRY THE CRON

--- server.js
+++ server.js
@@ -1,25 +1,65 @@
 require('dotenv').config();
 const http = require('http');
 const Twilio = require('twilio');
+const { MongoClient } = require('mongodb');
+const cron = require('node-cron');
 
 const twilioClient = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-const sendMessage = () => {
-  // Fetch information from MongoDB and send a message
-  // You'll need to implement the logic to retrieve data from your collection
-  const infoToBeSent = '...'; // Placeholder for actual info from MongoDB
-  // Send a message
-  twilioClient.messages.create({
-    to: process.env.MY_PHONE_NUMBER,
-    from: process.env.TWILIO_PHONE_NUMBER,
-    body: `Here is your daily update: ${infoToBeSent}`,
-  })
-    .then((message) => console.log(message.sid))
-    .catch((error) => console.error(error));
+// const sendMessage = () => {
+//   // Fetch information from MongoDB and send a message
+//   // You'll need to implement the logic to retrieve data from your collection
+//   const infoToBeSent = '...'; // Placeholder for actual info from MongoDB
+//   // Send a message
+//   twilioClient.messages.create({
+//     to: process.env.MY_PHONE_NUMBER,
+//     from: process.env.TWILIO_PHONE_NUMBER,
+//     body: `Here is your daily update: ${infoToBeSent}`,
+//   })
+//     .then((message) => console.log(message.sid))
+//     .catch((error) => console.error(error));
+// };
+
+
+// Function to fetch data from MongoDB and send a message
+const sendMessageWithDatabaseInfo = async () => {
+  const uri = process.env.MONGO_URI;
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+    await client.connect();
+    const database = client.db('Clusterfinix');
+    const collection = database.collection('questions');
+    
+    // Fetch the document containing the information
+    const document = await collection.findOne({});
+    const infoToBeSent = document.questionText; 
+
+    // Use the Twilio client to send a message
+    const message = await twilioClient.messages.create({
+      to: process.env.MY_PHONE_NUMBER,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      body: `Here is your daily update: ${infoToBeSent}`,
+    });
+
+    console.log('Message sent with ID:', message.sid);
+  } catch (err) {
+    console.error('Failed to retrieve data or send message:', err);
+  } finally {
+    await client.close();
+  }
 };
+
+cron.schedule('15 19 * * *', () => {
+  console.log('Running a job at 09:00 every day');
+  sendMessageWithDatabaseInfo();
+}, {
+  scheduled: true,
+  timezone: "Europe/London",
+});
 
 const server = http.createServer((req, res) => {
   if (req.url === '/send-test-message' && req.method === 'GET') {
-    sendMessage();  // Call the sendMessage function when this route is accessed
+    sendMessageWithDatabaseInfo();  // Call the sendMessage function when this route is accessed
     
     res.setHeader('Content-Type', 'text/plain');
     res.end('Triggered sendMessage function!');
